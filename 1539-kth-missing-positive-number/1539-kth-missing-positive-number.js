12a72f2dee25fe3c21f5b558e5a6e84fd0fdd7c3
Bicky Dutta <86409991+imbickydutta@users.noreply.github.com>
2022-08-23 19:47:06 +0530

    Time: 94 ms (54.77%), Space: 42.4 MB (57.27%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -9,16 +9,13 @@ var findKthPositive = function(arr, k) {
     }
     
     let count = k+1-arr[0];
-    
     for(let i=1;i<arr.length;i++){
-        if((arr[i] - arr[i-1]) <= count){
-            console.log(arr[i] - arr[i-1], count)
-            count-= arr[i] - (arr[i-1] + 1)
+        let diff = arr[i] - arr[i-1];
+        if(diff <= count){
+            count-= diff -1
         } else {
-            console.log("hello")
-           return (arr[i] - ((arr[i] - arr[i-1])-count)) 
+           return (arr[i] - (diff-count)) 
         }
     }
-    console.log(arr[arr.length-1] + count)
     return arr[arr.length-1] + count
 };
