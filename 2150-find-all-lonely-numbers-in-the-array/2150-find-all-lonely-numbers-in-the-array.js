de10c8ade1218e0fce74185b4f6417e0fa7069ee
Bicky Dutta <86409991+imbickydutta@users.noreply.github.com>
2022-08-24 18:34:35 +0530

    Time: 600 ms (37.96%), Space: 84.7 MB (67.59%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.js
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.js
@@ -3,16 +3,16 @@
  * @return {number[]}
  */
 var findLonely = function(nums) {
-    let obj = {};
-    for(let i=0;i<nums.length;i++){
-        obj[nums[i]] = (obj[nums[i]] || 0)+1
-    }
-    let arr=[];
-    for(let key in obj){
-        console.log(obj[key],obj[key-1],obj[key+1] )
-        if(obj[key]==1 && obj[+key-1]==undefined && obj[+key+1]==undefined){
-            arr.push(key)
+    nums.sort((a,b)=>a-b)
+    if(nums.length == 1) return nums
+    console.log(nums)
+    let newArr=[];
+    if(nums[1] != nums[0] && nums[1] != nums[0] + 1 ) newArr.push(nums[0])
+    if(nums[nums.length-1] != nums[nums.length-2] && nums[nums.length-1]!=nums[nums.length-2]+1) newArr.push(nums[nums.length-1])
+    for(let i=1;i<nums.length-1;i++){
+        if(nums[i-1] != nums[i]-1 && nums[i+1]!=nums[i]+1 && nums[i+1]!=nums[i] && nums[i-1] != nums[i] ){
+            newArr.push(nums[i])
         }
     }
-    return arr;
+    return(newArr)
 };
