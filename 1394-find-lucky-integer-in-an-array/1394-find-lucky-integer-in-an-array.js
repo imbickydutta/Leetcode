d2361c7ac7e447ef2c833471cd42a4183949b75b
Bicky Dutta <86409991+imbickydutta@users.noreply.github.com>
2022-08-24 17:45:38 +0530

    Time: 113 ms (33.97%), Space: 44.4 MB (38.73%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 var findLucky = function(arr) {
-    var ans = -1;
-    var obj = {};
-    for(var el of arr){
+    let ans = -1;
+    let obj = {};
+    for(let el of arr){
         obj[el] = (obj[el] || 0) + 1
     }
-    
-    var max = -Infinity;
-    
-    for(var key in obj){
+    let max = -Infinity;
+    for(let key in obj){
         if(key == obj[key] && key > max){
             max = +key
             ans = key
