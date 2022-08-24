1e04953d31251102f4bcb2ea84dbb0c9b2134f67
Bicky Dutta <86409991+imbickydutta@users.noreply.github.com>
2022-08-24 17:28:05 +0530

    Time: 119 ms (17.90%), Space: 42.4 MB (44.36%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -3,13 +3,10 @@
  * @return {boolean}
  */
 var canMakeArithmeticProgression = function(arr) {
-    arr = arr.sort((a,b)=>a-b);
-    console.log(arr)
+    arr.sort((a,b)=>a-b);
     let diff = arr[1] - arr[0];
     for(let i=0;i<arr.length-1;i++){
-        if(arr[i+1] -arr[i] != diff){
-            return false
-        }
+        if(arr[i+1] -arr[i] != diff) return false
     }
     return true
 };
